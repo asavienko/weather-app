33a1a4b13af8b2a577a0bbd455420f58eb5e260c
Anton <asavienko@gmail.com>
2020-05-29 14:28:43 +0300

    installed plugins

---
+++ src/App.styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const StyledWrapper = styled.div`
+  background: #f0f2f5;
+  height: 100%;
+  padding: 50px;
+  box-sizing: border-box;
+`;
+
+export const StyledLayout = styled.div`
+  background: #fff;
+  height: 100%;
+`;
